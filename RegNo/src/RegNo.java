c631d50c325c048e4d0bbaf5260771878b63eb48
Clint171 <clintsimiyu004@gmail.com>
2023-09-20 16:14:55 +0300

    Added Regno

---
+++ RegNo/src/RegNo.java
@@ -0,0 +1,14 @@
+
+
+public class RegNo {
+
+	public static void main(String[] args) {
+
+		float marks = 20.69f;
+		int amarks = (int)marks;
+		System.out.println(marks);
+		System.out.println(amarks);
+
+	}
+
+}
