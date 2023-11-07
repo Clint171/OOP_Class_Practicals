a7d40978207fa64cb54cccc87d27743f1b41f691
Clint171 <clintsimiyu004@gmail.com>
2023-11-07 18:02:10 +0300

    Broke the classes up

---
+++ Bank/src/dev/clint/CurrentAccount.java
@@ -0,0 +1,7 @@
+package dev.clint;
+
+public class CurrentAccount extends BankAccount {
+	public CurrentAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal) {
+		super(acNo, acName, acPhone, acEmail,AcBal);
+	}
+}
---
+++ GUI/src/dev/clint/Main.java
@@ -0,0 +1,21 @@
+package dev.clint;
+
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		createFrame();
+	}
+	public static void createFrame(){
+		JFrame frame = new JFrame("GUI ");
+		frame.setVisible(true);
+		createPanel();
+		
+	}
+	public static void createPanel(){
+		
+	}
+
+}
